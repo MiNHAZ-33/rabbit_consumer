d3f7695591cb02539849a028fdc3cfa6e03dd649
MiNHAZ-33 <miminhaz33@gmail.com>
2024-03-21 13:26:58 +0600

    added message count

--- app/services/consumer.rb
+++ app/services/consumer.rb
@@ -1,12 +1,13 @@
 class Consumer
   class << self
     def consume(queue_name)
-      queue = channel.queue(queue_name)
+      queue = channel.queue(queue_name, durable: true)
       # exchange = channel.fanout(queue_name)
       exchange = channel.direct("direct_exchange")
       queue.bind(exchange, routing_key: "direct_route")
       begin
         puts '[x x] waiting for message'
+        puts "[x] Counting" + queue.message_count.to_s
         queue.subscribe manual_ack: true do |delivery_info, _properties, body|
           puts "[x] received #{body}"
           body = JSON.parse!(body)
